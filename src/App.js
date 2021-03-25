14bef27809083e160074dd78365180bd02ac3e93
himzei <himzei@gmail.com>
2021-03-25 14:25:51 +0900

    8.8 Themes on Styled Components

--- src/App.js
+++ src/App.js
@@ -7,21 +7,35 @@ import {
 } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
-import { isLoggedInVar } from "./apollo";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+
+const lightTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "lightgray",
+};
+
+const darkTheme = {
+  fontColor: "lightgray",
+  bgColor: "#2c2c2c",
+};
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          {isLoggedIn ? <Home /> : <Login />}
-        </Route>
-        <Route>
-          <Redirect to="/" />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 export default App;
