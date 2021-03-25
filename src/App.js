af3a0b664e212c486e4e7aaf35a5bf1c70693bc5
himzei <himzei@gmail.com>
2021-03-25 13:30:39 +0900

    8.6 Reactive Valizbles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReactiveVar } from "@apollo/client";
 import {
   HashRouter as Router,
   Route,
@@ -7,19 +7,15 @@ import {
 } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import { isLoggedInVar } from "./apollo";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
     <Router>
       <Switch>
         <Route path="/" exact>
-          {isLoggedIn ? (
-            <Home setIsLoggedIn={setIsLoggedIn} />
-          ) : (
-            <Login setIsLoggedIn={setIsLoggedIn} />
-          )}
+          {isLoggedIn ? <Home /> : <Login />}
         </Route>
         <Route>
           <Redirect to="/" />
