7cf9615c781b00935ae6d7893d404f0066a668cd
himzei <himzei@gmail.com>
2021-03-25 20:11:56 +0900

    10.4 Sign Up UL

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,5 +1,74 @@
+import {
+  faFacebookSquare,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import AuthLayout from "../components/auth/AuthLayout";
+import BottomBox from "../components/auth/BottomBox";
+import Button from "../components/auth/Button";
+import FormBox from "../components/auth/FormBox";
+import Input from "../components/auth/Input";
+import Separator from "../components/auth/Separator";
+import { FatLink } from "../components/shared";
+import routes from "../routes";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Subtitle = styled(FatLink)`
+  font-size: 16px;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+const FacebookLogin = styled.div`
+  margin-top: 10px;
+  background-color: #385285;
+  width: 100%;
+  height: 100%;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  border-radius: 3px;
+  span {
+    margin-left: 10px;
+    font-weight: 600;
+    font-size: 12px;
+  }
+`;
+
 function SignUp() {
-  return null;
+  return (
+    <AuthLayout>
+      <FormBox>
+        <HeaderContainer>
+          <FontAwesomeIcon icon={faInstagram} size="3x" />
+          <Subtitle>
+            Sign up to see photos and videos from ypur friends
+          </Subtitle>
+        </HeaderContainer>
+        <FacebookLogin>
+          <FontAwesomeIcon icon={faFacebookSquare} />
+          <span>Log in With Facebook</span>
+        </FacebookLogin>
+        <Separator />
+        <form>
+          <Input type="text" placeholder="Email" />
+          <Input type="text" placeholder="Name" />
+          <Input type="text" placeholder="Username" />
+          <Input type="password" placeholder="Password" />
+          <Button type="submit" value="Sign up" />
+        </form>
+      </FormBox>
+      <BottomBox cta="Hava an account?" linkText="Log in" link={routes.home} />
+    </AuthLayout>
+  );
 }
 
 export default SignUp;
