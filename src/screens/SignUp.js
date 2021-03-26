e0b0bcdbbc1d352a8c1f7870b9034d1c7d08bcb7
himzei <himzei@gmail.com>
2021-03-26 11:43:10 +0900

    10.12 Create Account

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,14 +1,18 @@
+import { useMutation, gql } from "@apollo/client";
 import {
   faFacebookSquare,
   faGooglePlusSquare,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
 import Button from "../components/auth/Button";
 import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
 import Input from "../components/auth/Input";
 import Separator from "../components/auth/Separator";
 import SocialLogin from "../components/auth/SocialLogin";
@@ -37,7 +41,54 @@ const GoogleLogin = styled(SocialLogin)`
   background-color: #d7461f;
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 function SignUp() {
+  const history = useHistory();
+  const onCompleted = (data) => {
+    const {
+      createAccount: { ok, error },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    history.push(routes.home);
+  };
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, errors, formState } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
   return (
     <AuthLayout>
       <PageTitle title="Signup" />
@@ -57,12 +108,60 @@ function SignUp() {
           <span>Log in With Google</span>
         </GoogleLogin>
         <Separator />
-        <form>
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            ref={register({
+              required: "First Name is required",
+            })}
+            name="firstName"
+            type="text"
+            placeholder="First Name"
+          />
+          <FormError message={errors?.firstName?.message} />
+
+          <Input
+            ref={register}
+            name="lastName"
+            type="text"
+            placeholder="Last Name"
+          />
+          <FormError message={errors?.lastName?.message} />
+
+          <Input
+            ref={register({
+              required: "Email is required",
+            })}
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
+          <FormError message={errors?.email?.message} />
+
+          <Input
+            ref={register({
+              required: "Username is required",
+            })}
+            name="username"
+            type="text"
+            placeholder="Username"
+          />
+          <FormError message={errors?.username?.message} />
+
+          <Input
+            ref={register({
+              required: "Password is required",
+            })}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <FormError message={errors?.password?.message} />
+
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign up"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
       </FormBox>
       <BottomBox cta="Hava an account?" linkText="Log in" link={routes.home} />
