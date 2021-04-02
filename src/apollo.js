6d4706fe8e79e7ee36df69cf519c92bab6305107
himzei <himzei@gmail.com>
2021-04-03 08:33:00 +0900

    production url

--- src/apollo.js
+++ src/apollo.js
@@ -34,7 +34,10 @@ export const logUserOut = (token) => {
 export const darkModeVar = makeVar(Boolean(localStorage.getItem(DARK_MODE)));
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://instaclone-backend-himzei.herokuapp.com/graphql"
+      : "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
